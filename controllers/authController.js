5e4a9959df5a1f2d85fb676d3658096e40656e63
yanis <yanis.amine.harrat@gmail.com>
2025-02-21 00:05:37 +0100

    Update default user role ID in signup process

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ const signup = async (req, res) => {
         const { name, email, password } = req.body;
 
         // 🔹 Par défaut, un utilisateur a le rôle "user" (role_id = 0 d'après ta DB)
-        const defaultRoleId = 0;
+        const defaultRoleId = 7;
 
         // 🔹 Hachage du mot de passe
         const hashedPassword = await bcrypt.hash(password, 10);
