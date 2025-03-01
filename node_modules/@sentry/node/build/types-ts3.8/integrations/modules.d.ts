667cbef380568a75c296322d9b69af20f234b5ff
Yanis Harrat <yanis.amine.harrat@gmail.com>
2025-03-01 03:36:02 +0100

    init de la branche development

--- node_modules/@sentry/node/build/types-ts3.8/integrations/modules.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Add node modules / packages to the event.
- *
- * Only works in CommonJS (CJS) environments.
- */
-export declare const modulesIntegration: () => import("@sentry/core").Integration;
-//# sourceMappingURL=modules.d.ts.map
